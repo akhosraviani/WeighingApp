000ad7d43588a3f3ecd40cd765fa4c5aee23da56
Arash Khosraviani <arash.khosraviani@gmail.com>
2018-05-21 01:50:14 +0430

    update some parameters

--- DemoVideoRecorder/CreateWeighing.cs
+++ DemoVideoRecorder/CreateWeighing.cs
@@ -14,6 +14,8 @@ namespace AshaWeighing
     public partial class CreateWeighing : Form
     {
         private SqlConnection _dbConnection;
+        private SqlCommand sqlCommand;
+
         public CreateWeighing()
         {
             InitializeComponent();
@@ -201,8 +203,75 @@ namespace AshaWeighing
                 );
             if (res == System.Windows.Forms.DialogResult.Yes)
             {
-                // I have to cancel button click event here
+                sqlCommand = new SqlCommand("WMInv_000_InventoryWeighingOrder", _dbConnection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
 
+                // set up the parameters
+                sqlCommand.Parameters.Add("@GroupCode", SqlDbType.NVarChar, 64);
+                sqlCommand.Parameters.Add("@OrderTypeCode", SqlDbType.NVarChar, 64);
+                sqlCommand.Parameters.Add("@WeighingOrderCode", SqlDbType.NVarChar, 64);
+                sqlCommand.Parameters.Add("@TransactionTypeCode", SqlDbType.NVarChar, 64);
+                sqlCommand.Parameters.Add("@TransactionDateTime", SqlDbType.DateTime);
+                sqlCommand.Parameters.Add("@PartCode", SqlDbType.NVarChar, 64);
+                sqlCommand.Parameters.Add("@ReceivedQuantity", SqlDbType.Decimal);
+                sqlCommand.Parameters.Add("@IssuedQuantity", SqlDbType.Decimal);
+                sqlCommand.Parameters.Add("@UnitOfMeasureCode", SqlDbType.NVarChar, 64);
+                sqlCommand.Parameters.Add("@FromInventoryCode", SqlDbType.NVarChar, 64);
+                sqlCommand.Parameters.Add("@ToInventoryCode", SqlDbType.NVarChar, 64);
+                sqlCommand.Parameters.Add("@FromLocationCode", SqlDbType.NVarChar, 64);
+                sqlCommand.Parameters.Add("@ToLocationCode", SqlDbType.NVarChar, 64);
+                sqlCommand.Parameters.Add("@FromLotCode", SqlDbType.NVarChar, 64);
+                sqlCommand.Parameters.Add("@ToLotCode", SqlDbType.NVarChar, 64);
+                sqlCommand.Parameters.Add("@CostCenterCode", SqlDbType.NVarChar, 64);
+                sqlCommand.Parameters.Add("@CostAccountCode", SqlDbType.NVarChar, 64);
+                sqlCommand.Parameters.Add("@ContactCode", SqlDbType.NVarChar, 64);
+                sqlCommand.Parameters.Add("@DriverCode", SqlDbType.NVarChar, 64);
+                sqlCommand.Parameters.Add("@VehicleCode", SqlDbType.NVarChar, 64);
+                sqlCommand.Parameters.Add("@Note", SqlDbType.NVarChar, 64);
+                sqlCommand.Parameters.Add("@PersonnelCode", SqlDbType.NVarChar, 64);
+                sqlCommand.Parameters.Add("@PositionCode", SqlDbType.NVarChar, 64);
+                sqlCommand.Parameters.Add("@ComponentCode", SqlDbType.NVarChar, 64);
+                sqlCommand.Parameters.Add("@CreatorCode", SqlDbType.NVarChar, 64);
+                sqlCommand.Parameters.Add("@ReturnMessage", SqlDbType.NVarChar, 1024).Direction = ParameterDirection.Output;
+                sqlCommand.Parameters.Add("@ReturnValue", SqlDbType.Int).Direction = ParameterDirection.Output;
+
+                // set parameter values
+                sqlCommand.Parameters["@OrderTypeCode"].Value = cmbWeighingTypes.SelectedValue;
+                sqlCommand.Parameters["@WeighingTypeCode"].Value = cmbWeighingTypes.SelectedValue;
+                sqlCommand.Parameters["@OperationCode"].Value = cmbOperationCode.SelectedValue;
+                sqlCommand.Parameters["@Weight"].Value = int.Parse(sevenSegmentWeight.Value);
+                sqlCommand.Parameters["@Image1"].Value = (object)imageToByteArray(imgCamera1.Image) ?? DBNull.Value;
+                sqlCommand.Parameters["@Image2"].Value = (object)imageToByteArray(imgCamera2.Image) ?? DBNull.Value;
+                sqlCommand.Parameters["@Image3"].Value = (object)imageToByteArray(imgCamera3.Image) ?? DBNull.Value;
+                sqlCommand.Parameters["@Image4"].Value = (object)imageToByteArray(imgCamera4.Image) ?? DBNull.Value;
+                sqlCommand.Parameters["@MachineCode"].Value = Globals.WeighingMachineCode;
+                sqlCommand.Parameters["@ResponsibleCode"].Value = Globals.PersonnelCode;
+                sqlCommand.Parameters["@CreatorCode"].Value = Globals.UserCode;
+                sqlCommand.Parameters["@ReturnMessage"].Value = "";
+                sqlCommand.Parameters["@ReturnValue"].Value = 1;
+
+                sqlCommand.ExecuteNonQuery();
+                string returnMessage = Convert.ToString(sqlCommand.Parameters["@ReturnMessage"].Value);
+                int returnValue = Convert.ToInt32(sqlCommand.Parameters["@ReturnValue"].Value);
+
+                LogHelper.Log(LogTarget.Database, "Database", _weighingOrderCode, "btnSaveData_Click: Weighing data saved. " +
+                            "WeighingOrderCode=" + _weighingOrderCode +
+                            ", CreatorCode=" + Globals.UserName +
+                            ", SavedWeight=" + sevenSegmentWeight.Value +
+                            ", IndicatorWeight=" + _indicatorWeight, Globals.UserCode);
+
+                if (returnValue == 1)
+                {
+                    MessageBox.Show(returnMessage, "پیغام", MessageBoxButtons.OK,
+                        MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+                    sqlCommand.Dispose();
+                }
+                else if (returnValue == 0)
+                {
+                    MessageBox.Show(returnMessage, "اخطار", MessageBoxButtons.OK,
+                        MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                    sqlCommand.Dispose();
+                }
             }
         }
     }
